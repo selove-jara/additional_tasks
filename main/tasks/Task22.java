861f2d548f2d92b24ecd48de047144c012980cfa
selov <selove.jara@gmail.com>
2024-05-23 17:32:22 +0400

    Форматированный вывод

---
+++ main/tasks/Task22.java
@@ -0,0 +1,9 @@
+package main.tasks;
+
+public class Task22 {
+    public static void main(String[] args) {
+        String name = "Andrey";
+        String result = String.format("Привет, %s!", name);
+        System.out.println(result);
+    }
+}
